3c0f4a64ec37b0647ac8da1632075aaa148a34b9
trieu nguyen <minhtrieu2720@outlook.com>
2020-12-03 04:42:54 +0700

    add db design

---
+++ eShopSolutions.Data/Entities/ProductInCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolutions.Data.Entities
+{
+    public class ProductInCategory
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
