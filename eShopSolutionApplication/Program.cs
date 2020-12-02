8195668abfd47c333fe9e39f652f71bee464b814
trieu nguyen <minhtrieu2720@outlook.com>
2020-12-03 06:11:35 +0700

    add entity framework

---
+++ eShopSolutionApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eShopSolutionApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ eShopSolutions.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eShopSolutions.Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
