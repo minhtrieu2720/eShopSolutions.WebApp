79c5a8df53baaa8432f315217f2a7c9a343b91a4
trieu nguyen <minhtrieu2720@outlook.com>
2020-12-03 00:43:32 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eShopSolutions.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
